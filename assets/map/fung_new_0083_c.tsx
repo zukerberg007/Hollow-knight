722acafb6e6a9f92132b1062d3ede40813026ea0
Amirali zakeri <Amirzakeri457@gmail.com>
2026-07-03 02:50:53 +0330

    Update map: add fung_new_0083_c tileset

---
+++ assets/map/fung_new_0083_c.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="fung_new_0083_c" tilewidth="8" tileheight="8" tilecount="1650" columns="55">
+ <image source="../Greenpath/fung_new_0083_c.png" width="443" height="243"/>
+</tileset>
